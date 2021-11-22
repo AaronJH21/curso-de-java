d8af7025086f27244168dc002788d7b87225200a
AaronJH21 <93291172+AaronJH21@users.noreply.github.com>
2021-11-22 16:24:10 -0300

    Add files via upload

---
+++ javascrip/script.js
@@ -0,0 +1,11 @@
+console.log("hola, perdon por ser tardar tanto")
+let numero1 = Number(prompt("ingresa un numero para sumar")) 
+let numero2 = parseFloat(prompt("ingresa un segundo numero"))
+let numero3 = parseInt(prompt("ingresa un tercer numero")) 
+
+let resultado = (numero1+numero2+numero3)
+
+console.log("El resultado es :"+resultado)
+alert("El resultado es:"+resultado )
+/* se que soio se debe usar una se que no debi usar  Number, parseFloat , parseInt en el ejercicio ya que son sirven para lo mismo pero queria que fuera un poco mas original por eso lo coloque asi
+la proxima solo usare uno (igual lo porbe y funcionna asi que por eso lo puse asi)*/
